cc89fb91dbb22dedf0da6e49d8a14ff358adcd0d
Young rich <tajiriramadhan@gmail.com>
2024-03-26 08:00:07 +0300

    the first phase using normal composition API

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <Header/>
+
+        <div class="container">
+            <Balance />
+            <IncomeExpences />
+            <TransactionList />
+            <AddTransaction />
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from '../components/Header.vue';
+    import Balance from '../components/Balance.vue';
+    import IncomeExpences from '../components/IncomeExpences.vue';
+    import TransactionList from '../components/TransactionList.vue';
+    import AddTransaction from '../components/AddTransaction.vue';
+
+
+
+    export default {
+        components: {
+            Header,
+            Balance,
+            IncomeExpences,
+            TransactionList,
+            AddTransaction
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
